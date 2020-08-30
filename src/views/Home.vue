1fc6f488bb5d1016be5eb68499009c4a024ec404
juanbarrios18 <juanf18889@gmail.com>
2020-08-30 18:29:17 +0200

    project complete - deploying in Netlify

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    <Px-assets-table v-if="!isLoading" v-bind:assets="assets" />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable";
+import api from "@/api";
+
+export default {
+  name: "Home",
+  components: { PxAssetsTable },
+
+  data() {
+    return {
+      assets: [],
+      isLoading: false
+    };
+  },
+
+  created() {
+    this.isLoading = true;
+    api
+      .getAssets()
+      .then(assets => (this.assets = assets))
+      .finally(() => (this.isLoading = false));
+  }
+};
+</script>
